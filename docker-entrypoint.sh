3a1b76d7db22290dea011776ab0036b5044f21e5
azkadani214 <dharmazka1980@gmail.com>
2026-07-06 20:23:09 +0700

    dockerfile error fixNN

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,26 +1,12 @@
 #!/bin/sh
 set -e
 
-SETUP_FLAG="/var/www/html/storage/.setup_completed"
+php artisan optimize:clear
 
-if [ ! -f "$SETUP_FLAG" ]; then
-    echo "⚙️ Menjalankan inisialisasi awal production..."
+php artisan storage:link || true
 
-    # Membuat symlink storage (Aman dijalankan)
-    echo "🔗 Linking storage..."
-    php artisan storage:link || true
-
-    # Optimasi Laravel untuk Production (Bisa dijalankan di sini karena config/route bergantung environment .env runtime)
-    echo "🚀 Optimizing Laravel..."
-    php artisan optimize || true
-    php artisan view:cache || true
-
-    touch "$SETUP_FLAG"
-    echo "✅ Inisialisasi awal selesai."
-else
-    echo "✅ Setup sudah dilakukan sebelumnya, melewati inisialisasi..."
-fi
-
-# Migrasi Dihapus. Lakukan migrasi secara manual: `docker exec app php artisan migrate --force`
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 
 exec "$@"
